deb8ea0cdab8b1eca21c8a0b0c7ff86782b41074
Justin Hoyt <justinhoyt24@gmailcom>
2018-07-16 19:15:44 +0000

    Compelete sherlock string with all tests passing

--- JustinHackerRank/sherlock_and_the_valid_string.py
+++ JustinHackerRank/sherlock_and_the_valid_string.py
@@ -1,32 +1,35 @@
+# https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
 class valid_string:
-    def is_valid(self, word):
-        mymap = {}
-        for char in word:
-            if char in mymap:
-                mymap[char] = mymap[char] + 1
-            else:
-                mymap[char] = 1
+def is_valid(self, word):
+    mymap = {}
+    for char in word:
+        if char in mymap:
+            mymap[char] = mymap[char] + 1
+        else:
+            mymap[char] = 1
 
-        occurances_map = {}
-        for key in mymap:
-            if mymap[key] in occurances_map:
-                occurances_map[mymap[key]] = occurances_map[mymap[key]] + key
-            else:
-                occurances_map[mymap[key]] = key
+    occurances_map = {}
+    for key in mymap:
+        if mymap[key] in occurances_map:
+            occurances_map[mymap[key]] = occurances_map[mymap[key]] + key
+        else:
+            occurances_map[mymap[key]] = key
 
-        # print(occurances_map)
-        if len(occurances_map) == 1:
+    if len(occurances_map) == 1:
+        return "YES"
+    elif len(occurances_map) == 2:
+        if 1 in occurances_map and len(occurances_map[1]) == 1:
             return "YES"
-        elif len(occurances_map) == 2:
-            vals = []
-            for num_occurances in occurances_map:
-                vals.append(num_occurances)
-            if abs(vals[0] - vals[1]) == 1:
-                if len(occurances_map[vals[0]]) > 1 and len(occurances_map[vals[1]]) > 1:
-                    return "NO"
-                else:
-                    return "YES"
+        occurances = []
+        for num_occurances in occurances_map:
+            occurances.append(num_occurances)
+            occurances.sort()
+        if occurances[1] - occurances[0] == 1:
+            if len(occurances_map[occurances[1]]) == 1 and len(occurances_map[occurances[0]]) > 1:
+                return "YES"
             else:
                 return "NO"
         else:
             return "NO"
+    else:
+        return "NO"
--- JustinHackerRank/test_sherlock_and_the_valid_string.py
+++ JustinHackerRank/test_sherlock_and_the_valid_string.py
@@ -7,13 +7,17 @@ class TestStringMethods(unittest.TestCase):
       validate = valid_string()
       self.assertEqual(validate.is_valid("aabbcc"), "YES")
 
-  def test_off_by_one_number_of_occurances(self):
+  def test_two_chars_over_by_one(self):
       validate = valid_string()
       self.assertEqual(validate.is_valid("aabbc"), "YES")
 
   def test_off_by_one_number_of_occurances_twice(self):
       validate = valid_string()
       self.assertEqual(validate.is_valid("aabbcd"), "NO")
+
+  def test_one_char_over_by_one_occurance(self):
+      validate = valid_string()
+      self.assertEqual(validate.is_valid("aabcd"), "YES")
 
 if __name__ == '__main__':
     unittest.main()
