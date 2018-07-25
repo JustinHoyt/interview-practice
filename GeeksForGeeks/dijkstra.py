489d01174c217702809852f5251d175bb51fd5a2
Justin Hoyt <justinhoyt24@gmailcom>
2018-07-25 18:04:38 +0000

    Add dijkstra's problem with initialized graph

---
+++ GeeksForGeeks/dijkstra.py
@@ -0,0 +1,23 @@
+class Graph:
+    def __init__(self, size):
+        self.graph = [0 for col in range(size) for row in range(size)]
+
+    def dijkstra(self, graph):
+        visited_set = set()
+
+
+g = Graph(9)
+g.graph = [
+    [0, 4, 0, 0, 0, 0, 0, 8, 0],
+    [4, 0, 8, 0, 0, 0, 0, 11, 0],
+    [0, 8, 0, 7, 0, 4, 0, 0, 2],
+    [0, 0, 7, 0, 9, 14, 0, 0, 0],
+    [0, 0, 0, 9, 0, 10, 0, 0, 0],
+    [0, 0, 4, 14, 10, 0, 2, 0, 0],
+    [0, 0, 0, 0, 0, 2, 0, 1, 6],
+    [8, 11, 0, 0, 0, 0, 1, 0, 7],
+    [0, 0, 2, 0, 0, 0, 6, 7, 0]
+]
+
+graph.dijkstra(0)
+print(graph.graph)
