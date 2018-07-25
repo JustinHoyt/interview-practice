03eb0889f69821794e1780a8aaebdda34365d132
Justin Hoyt <justinhoyt24@gmailcom>
2018-07-25 17:56:52 +0000

    Add rotate 90 degree in place problem and solution

---
+++ GeeksForGeeks/rotate_90_degrees.py
@@ -0,0 +1,24 @@
+def rotate_90_in_place(nums):
+    size = len(nums[0])
+    for i in range(0, size-1):
+        for j in range(i, size-1-i):
+            p1 = nums[i][j]
+            p2 = nums[j][size-1-i]
+            p3 = nums[size-1-i][size-1-j]
+            p4 = nums[size-1-j][i]
+            nums[j][size-1-i] = p1
+            nums[size-1-i][size-1-j] = p2
+            nums[size-1-j][i] = p3
+            nums[i][j] = p4
+
+
+matrix = [
+    [1, 6, 7, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
+rotate_90_in_place(matrix)
+for i in range(len(matrix[0])):
+    print(matrix[i])
