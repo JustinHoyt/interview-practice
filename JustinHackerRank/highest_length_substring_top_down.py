c0691cae8094eb02b46a59ac49b4b06f6368b73f
Justin Hoyt <justinhoyt24@gmailcom>
2018-08-08 03:01:06 +0000

    Further in algorithm

--- JustinHackerRank/highest_length_substring_top_down.py
+++ JustinHackerRank/highest_length_substring_top_down.py
@@ -1,27 +1,41 @@
-def high_substr_rec(str1, str2, str1_end, str2_end, memo):
-    key = str(str1_end) + "-" + str(str2_end) + "-" + str()
+def high_substr_rec(str1, str2, start1, start2, end1, end2, memo, found):
+
+    key = str(start1) + "-" + str(end1) + " " + str(start1) + "-" + str(end2)
     if key in memo:
         return memo[key]
     high = 0
-    if str1_end == 0 or str2_end == 0:
-        if str1[str1_end] == str2[str2_end]:
-            return 1
-        else:
-            return 0
+    if start1 == end1 or start2 == end2:
+        return 0
 
-    opt1 = high_substr_rec(str1, str2, str1_end-1, str2_end, memo)
-    opt2 = high_substr_rec(str1, str2, str1_end, str2_end-1, memo)
-    if str1[str1_end] == str2[str2_end]:
+    if found:
+        return 0
+    opt1 = high_substr_rec(str1, str2, start1+1, start2, end1, end2, memo, found)
+    if str1[start1] == str2[start2]:
         opt1 += 1
+        found = True
+    opt2 = high_substr_rec(str1, str2, start1, start2+1, end1, end2, memo, found)
+    if str1[start1] == str2[start2]:
         opt2 += 1
+        found = True
+    opt3 = high_substr_rec(str1, str2, start1, start2, end1-1, end2, memo, found)
+    if str1[end1] == str2[end2]:
+        opt3 += 1
+        found = True
+    opt4 = high_substr_rec(str1, str2, start1, start2, end1, end2-1, memo, found)
+    if str1[end1] == str2[end2]:
+        opt4 += 1
+        found = True
 
-    high = max(opt1, opt2)
+    high = max(opt1, opt2, opt3, opt4)
     memo[key] = high
     return high
 
 
 def high_substr(str1, str2):
-    return high_substr_rec(str1, str2, len(str1)-1, len(str2)-1, {})
+    found = False
+    return high_substr_rec(str1, str2, 0, 0, len(str1)-1, len(str2)-1, {}, found)
 
 
-print(high_substr("shinchan", "noharaaa"))
+# print(high_substr("shinchan", "noharaaa"))
+print(high_substr("abdcc", "abcde"))
+
