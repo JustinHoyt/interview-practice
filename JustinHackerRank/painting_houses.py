64f6ddc1de53f1e9f7a2302c282a0745bf5ab78b
Justin Hoyt <justinhoyt24@gmailcom>
2018-08-12 02:45:28 +0000

    Compiles and runs

--- JustinHackerRank/painting_houses.py
+++ JustinHackerRank/painting_houses.py
@@ -1,27 +1,32 @@
+import math
+
+
 def paint(houses, memo, house, paint_color):
-    key = str(house) + "-" + str(paint_color)
+    key = str(house) + ", " + str(paint_color)
     if key in memo:
         return memo[key]
 
     if house == len(houses)-1:
         return houses[house][paint_color]
 
-    lowest_cost = float('inf')
+    lowest_cost = math.inf
     for i in range(3):
         if paint_color != i:
-            possible_cost = paint(houses, memo, houses+1, i)
-            possible_cost += houses[house][i]
+            possible_cost = paint(houses, memo, house+1, i) + houses[house][i]
             lowest_cost = min(possible_cost, lowest_cost)
 
     memo[key] = lowest_cost
+    print(memo)
     return lowest_cost
 
 
 def paint_houses(houses):
     memo = {}
     house = 0
-    paint_color = -1
-    paint(houses, memo, house, paint_color)
+    result = math.inf
+    for paint_color in range(3):
+        result = min(paint(houses, memo, house, paint_color), result)
+    return result
 
 
 print(paint_houses([[17, 2, 17], [16, 16, 5], [14, 3, 19]]))
