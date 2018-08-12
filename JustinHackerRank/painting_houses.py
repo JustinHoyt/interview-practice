b34a77d7a8c77063f5a8fb1fca311c4e226b4535
Justin Hoyt <justinhoyt24@gmailcom>
2018-08-12 03:04:25 +0000

    Solved painting_houses problem

--- JustinHackerRank/painting_houses.py
+++ JustinHackerRank/painting_houses.py
@@ -12,11 +12,11 @@ def paint(houses, memo, house, paint_color):
     lowest_cost = math.inf
     for i in range(3):
         if paint_color != i:
-            possible_cost = paint(houses, memo, house+1, i) + houses[house][i]
+            possible_cost = paint(houses, memo, house+1, i)
+            possible_cost += houses[house][paint_color]
             lowest_cost = min(possible_cost, lowest_cost)
 
     memo[key] = lowest_cost
-    print(memo)
     return lowest_cost
 
 
@@ -24,6 +24,7 @@ def paint_houses(houses):
     memo = {}
     house = 0
     result = math.inf
+    # result = paint(houses, memo, house, -1)
     for paint_color in range(3):
         result = min(paint(houses, memo, house, paint_color), result)
     return result
