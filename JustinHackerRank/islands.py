e236ed66ee2aa96e5fd0374e2dc50c5ebbc6c527
Justin Hoyt <justinhoyt24@gmailcom>
2018-09-02 14:32:10 +0000

    Add islands solution with is_visited_grid

--- JustinHackerRank/islands.py
+++ JustinHackerRank/islands.py
@@ -1,3 +1,28 @@
+def explore_land_with_is_visited_grid(grid, is_visited_grid, row, col):
+    if row < len(grid) and \
+            row >= 0 and \
+            col < len(grid[0]) and \
+            col >= 0 and \
+            grid[row][col] == 1 and \
+            is_visited_grid[row][col] == 0:
+        is_visited_grid[row][col] = 1
+        explore_land_with_is_visited_grid(grid, is_visited_grid, row+1, col)
+        explore_land_with_is_visited_grid(grid, is_visited_grid, row-1, col)
+        explore_land_with_is_visited_grid(grid, is_visited_grid, row, col+1)
+        explore_land_with_is_visited_grid(grid, is_visited_grid, row, col-1)
+
+
+def count_islands_with_is_visited_grid(grid):
+    is_visited_grid = [[0] * len(grid[0]) for x in range(len(grid))]
+    islands_count = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1 and is_visited_grid[row][col] == 0:
+                islands_count += 1
+                explore_land_with_is_visited_grid(grid, is_visited_grid, row, col)
+    return islands_count
+
+
 def explore_land(grid, row, col):
     if row < len(grid) and \
             row >= 0 and \
@@ -20,7 +45,6 @@ def count_islands(grid):
                 islands_count += 1
     return islands_count
 
-
 grid = [[1, 1, 0, 1],
         [0, 1, 1, 1],
         [1, 1, 0, 0],
@@ -28,4 +52,5 @@ grid = [[1, 1, 0, 1],
         [1, 1, 1, 1],
 ]
 
+print(count_islands_with_is_visited_grid(grid))
 print(count_islands(grid))
