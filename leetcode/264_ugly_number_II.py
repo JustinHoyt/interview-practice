0d89ea0133f015b6d25aaf6098783fc48aa2c8c5
Justin Hoyt <justinhoyt24@gmailcom>
2018-10-16 02:04:07 +0000

    Add ugly number II

---
+++ leetcode/264_ugly_number_II.py
@@ -0,0 +1,31 @@
+def nth_ugly_num(n):
+    memo = [True, True, True, True, True]
+
+    num = 5
+    count = 5
+    while count != n+1:
+        if num % 2 == 0:
+            memo.append(memo[num//2])
+            if memo[-1] is True:
+                count += 1
+        elif num % 3 == 0:
+            memo.append(memo[num//3])
+            if memo[-1] is True:
+                count += 1
+        elif num % 5 == 0:
+            memo.append(memo[num//5])
+            if memo[-1] is True:
+                count += 1
+        else:
+            memo.append(False)
+        num += 1
+    return len(memo) - 1
+
+
+print(nth_ugly_num(11))
+print(nth_ugly_num(12))
+print(nth_ugly_num(13))
+print(nth_ugly_num(14))
+print(nth_ugly_num(15))
+print(nth_ugly_num(16))
+print(nth_ugly_num(17))
