817b74017a269fb515df4227a7c6ae87199e2473
Justin Hoyt <justinhoyt24@gmail.com>
2020-01-25 14:35:46 -0500

    Add explaination of dictionary wildcard

--- JustinHackerRank/dictionary_wildcard.js
+++ JustinHackerRank/dictionary_wildcard.js
@@ -1,3 +1,17 @@
+/*
+ You are given an array of words and a string query. You must return true or false
+ depending on whether the array of words match the query or not. A query may contain
+ the wildcard character "*" which can match with any character at that spot.
+
+Examples:
+isMember(["foo", "bar", "baz"], "f*o")
+will return true since the "*" matches the first "o" in foo
+
+
+isMember(["foo", "bar", "baz"], "**")
+will return false since there are no two-letter words in the array
+*/
+
 const isMatchingWord = (isMatchedSoFar, [wordChar, queryChar]) => {
     return isMatchedSoFar && (queryChar === wordChar || queryChar === "*");
 };
