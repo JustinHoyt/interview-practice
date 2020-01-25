12f33cb72ec00b5c05eb3b08dd301476b3b9fb74
Justin Hoyt <justinhoyt24@gmail.com>
2020-01-25 14:59:22 -0500

    Update dictionary_wildcard problem

--- JustinHackerRank/dictionary_wildcard.js
+++ JustinHackerRank/dictionary_wildcard.js
@@ -12,24 +12,19 @@ isMember(["foo", "bar", "baz"], "**")
 will return false since there are no two-letter words in the array
 */
 
-const isMatchingWord = (isMatchedSoFar, [wordChar, queryChar]) => {
-    return isMatchedSoFar && (queryChar === wordChar || queryChar === "*");
-};
-
-const isMatchInWords = (isMatchInArray, [word, query]) => {
+const match = ([word, query]) => {
     const zippedWordAndQuery = Array.from(word).map((wordChar, i) => [wordChar, query[i]]);
-    const matched = zippedWordAndQuery.reduce(isMatchingWord, true);
-
-    return matched || isMatchInArray;
+    return zippedWordAndQuery.every(([wordChar, queryChar]) => {
+        return queryChar === wordChar || queryChar === "*";
+    });
 };
 
 const dictionary_wildcard = (words, query) => {
     const filteredWords = words.filter( word => word.length == query.length);
     const zippedWordsAndQuery = filteredWords.map((word) => [word, query]);
 
-    return zippedWordsAndQuery.reduce( isMatchInWords, false);
+    return zippedWordsAndQuery.some(match);
 };
-
 
 const words = ["foo", "bar", "baz"];
 console.log(dictionary_wildcard(words, "b*r"));
