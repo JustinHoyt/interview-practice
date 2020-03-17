12301badf65a268489dee35ee0840b080733cbec
Justin Hoyt <justinhoyt24@gmail.com>
2020-03-17 11:05:33 -0400

    Refactor dictionary_wildcard

--- HackerRank/dictionary_wildcard.py
+++ HackerRank/dictionary_wildcard.py
@@ -20,46 +20,37 @@ def is_match(query, word):
 print(is_member(["foo", "bar", "baz"], "foo"))
 
 
-class TrieNode:
-    def __init__(self, last_letter=False):
-        self.children = defaultdict(TrieNode)
-        self.last_letter = last_letter
 
 class Dictionary:
     def __init__(self, words):
-        self.trie = TrieNode()
+        self.trie = defaultdict(dict)
         [ self.add(word) for word in words ]
         
     def is_match(self, query):
-
         def word_match(node, idx=0):
-            letter = query[idx]
-            if idx == len(query) - 1:
-                if letter == "*":
-                    return len(node.children) > 0
-
-                return node.children[letter].last_letter
+            if idx == len(query):
+                return "." in node
             
+            letter = query[idx]
             if query[idx] == "*":
-                return True in [ word_match(child, idx+1) for child in node.children.values() ]
+                return True in [ word_match(child, idx+1) for child in node.values() ]
 
-            if letter not in node.children:
+            if letter not in node:
                 return False
 
-            return word_match(node.children[letter], idx+1)
+            return word_match(node[letter], idx+1)
             
         return word_match(self.trie)
 
     def add(self, word):
         curr = self.trie
         for idx, letter in enumerate(word):
-            if letter not in curr.children:
-                curr.children[letter] = TrieNode()
+            if letter not in curr:
+                curr[letter] = {}
 
-            if idx == len(word) - 1:
-                curr.children[letter].last_letter = True
-            
-            curr = curr.children[letter]
+            curr = curr[letter]
+
+        curr["."] = {}
 
 dictionary = Dictionary(["foo", "bar", "baz"])
 print(dictionary.is_match("foo"))
