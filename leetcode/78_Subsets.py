c439e76e8091195ed7835ef08627fb17a4cfa588
Justin Hoyt <justinhoyt24@gmail.com>
2020-04-19 23:36:15 -0400

    Add subsets problem

--- leetcode/78_Subsets.py
+++ leetcode/78_Subsets.py
@@ -1,35 +1,24 @@
+from typing import List
+import sys
+# sys.setrecursionlimit(1500)
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = [[]]
+
+        def generate_subsets(subset, i):
+            if i == len(nums):
+                result.append(subset)
+                return
+
+            generate_subsets(subset, i + 1)
+            new_subset = subset[:]
+            new_subset.append(nums[i])
+            generate_subsets(new_subset, i + 1)
+
         for i, num in enumerate(nums):
-            pass
+            generate_subsets(nums[i:i+1], i+1)
+
+        return result
 
 print(Solution().subsets([1,2,3]))
-
-'''
-   R
- L
-[1,2,3]
-
-[
-  []
-  [1]
-  [1,2]
-  [1,2,3]
-  [1,3]
-  [2]
-  [2,3]
-  [3]
-]
-
-[
-  [3],
-  [1],
-  [2],
-  [1,2,3],
-  [1,3],
-  [2,3],
-  [1,2],
-  []
-]
-'''
