921c7d7eae914be7fee0fee207863711230d7623
Justin Hoyt <justinhoyt24@gmail.com>
2020-05-01 01:16:21 -0400

    Add evaluate_division problem

--- leetcode/evaluate_division.py
+++ leetcode/evaluate_division.py
@@ -1,50 +1,36 @@
-def find_quotient(graph, start, end, visited):
-    if start == end:
-        return 1
-    visited.add(start)
-    for edge, weight in graph.items():
-        if edge[0] == start and edge[1] not in visited:
-            temp = find_quotient(graph, edge[1], end, visited)
-            if temp != -1:
-                return temp * weight
-    return -1
-
-
-graph = {
-    ('a','b'): 2.0,
-    ('b','a'): 1/2.0,
-    ('b','c'): 3.0,
-    ('c','b'): 1/3.0,
-}
-print(find_quotient(graph, 'a', 'c', set()))
-
-graph = {
-    'a': {'b': 2.0}
-}
+from collections import defaultdict
+from typing import List
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         result = []
 
         def make_graph(equations, values):
-            pass
+            graph = defaultdict(dict)
+            for equation, value in zip(equations, values):
+                start, end = equation
+                graph[start][end] = value
+                graph[end][start] = 1/value
+            return graph
 
         graph = make_graph(equations, values)
+        print(graph)
 
-        def add_quotient(start, end, visited, sofar):
-            if start == end:
-                return sofar
+        def add_quotient(start, end, visited, sofar=1):
+            if start == end and end in graph:
+                result.append(sofar)
             visited.add(start)
 
             for node, weight in graph[start].items():
                 if node not in visited:
-                    quotient = add_quotient(node, end, visited, sofar + weight)
-                    if quotient != -1:
-                        return quotient
-            return -1
+                    add_quotient(node, end, visited, sofar * weight)
 
         for start, end in queries:
-            add_quotient(start, end, graph, set())
+            size = len(result)
+            add_quotient(start, end, set())
+            if len(result) == size:
+                result.append(-1)
 
         return result
 
+print(Solution().calcEquation([ ["a", "b"], ["b", "c"] ], [2.0, 3.0], [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]))
