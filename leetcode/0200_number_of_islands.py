a5e11bfde154955e5497853601261467971c254b
Justin Hoyt <justinhoyt24@gmail.com>
2021-09-16 01:03:43 -0700

    Rewrite number of islands

--- leetcode/0200_number_of_islands.py
+++ leetcode/0200_number_of_islands.py
@@ -1,37 +1,51 @@
-from typing import List
-
+from typing import *
 
 class Solution:
-    def numIslands(self, matrix) -> int:
-        if len(matrix) == 0:
-            return 0
-        visited = {}
+    def numIslands(self, grid: List[List[str]]) -> int:
         num_islands = 0
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        def mark_island(i, j):
+            if (
+                0 <= i < len(grid) and
+                0 <= j < len(grid[0]) and
+                grid[i][j] == '1'
+            ):
+                grid[i][j] = 0
+                for i_offset, j_offset in [[-1,0], [0,-1], [1,0], [0,1]]:
+                    mark_island(i + i_offset,j + j_offset)
+            
 
-        def explore_island(i, j):
-            if (0 <= i < len(matrix) and
-                    0 <= j < len(matrix[i]) and
-                    matrix[i][j] == 1 and
-                    (i, j) not in visited):
-                visited[(i, j)] = 1
-                for i_offset, j_offset in directions:
-                    explore_island(i + i_offset, j + j_offset)
-
-        for i, row in enumerate(matrix):
+        for i, row in enumerate(grid):
             for j, cell in enumerate(row):
-                if cell == 1 and (i, j) not in visited:
-                    explore_island(i, j)
+                if cell == '1':
+                    mark_island(i, j)
                     num_islands += 1
 
         return num_islands
 
 
-sol = Solution()
-matrix = [
-    [1, 0, 1],
-    [1, 1, 1],
-    [0, 0, 0],
-    [0, 1, 0]
-]
-print(sol.numIslands(matrix))
+def test_happy_path():
+    matrix = [
+        ['1', '0', '1'],
+        ['1', '1', '1'],
+        ['0', '0', '0'],
+        ['0', '1', '0']
+    ]
+    assert Solution().numIslands(matrix) == 2
+
+
+def test_single_island():
+    matrix = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+    assert Solution().numIslands(matrix) == 1
+
+
+
+if __name__ == "__main__":
+    test_happy_path()
+
+
+
