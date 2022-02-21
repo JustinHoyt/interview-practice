9a5054efc28f0fa9bed10c7791ab01d651449c19
Justin Hoyt <justinhoyt24@gmail.com>
2022-02-20 22:00:06 -0800

    Add itertools and functools examples

---
+++ Python/functools.py
@@ -0,0 +1,35 @@
+from functools import partial, reduce, cache, lru_cache
+from operator import add, mul
+
+def test_pipe():
+    add_one = partial(add, 1)
+    double = partial(mul, 2)
+
+
+    def pipe(*functions):
+        return reduce(lambda f, g: lambda x: g(f(x)), functions, lambda x: x)
+
+    operations = [add_one, double, double]
+
+    assert pipe(
+        partial(map, pipe(*operations)),
+        list,
+    )([1,2,3,4]) == [8,12,16,20]
+
+
+def test_cache():
+    @cache
+    def fib(n):
+        return 1 if n <= 1 else fib(n - 1) + fib(n - 2)
+
+    print(fib(100))
+
+    @lru_cache
+    def fib(n):
+        return 1 if n <= 1 else fib(n - 1) + fib(n - 2)
+
+    print(fib(100))
+
+if __name__ == "__main__":
+    test_pipe()
+
