159065f9a2cbb36683b15bb0ec93ab079bb80ca3
Justin Hoyt <justinhoyt24@gmail.com>
2022-02-24 02:41:17 -0800

    Add getattrs example

--- Python/functools.py
+++ Python/functools.py
@@ -1,6 +1,13 @@
 from functools import partial, reduce, cache, lru_cache
 from operator import add, mul
 
+class Node:
+    def __init__(self, key: int, val: int, left=None, right=None):
+        self.val = val
+        self.key = key
+        self.left = left
+        self.right = right
+
 def test_pipe():
     add_one = partial(add, 1)
     double = partial(mul, 2)
@@ -14,6 +21,21 @@ def test_pipe():
         partial(map, pipe(*operations)),
         list,
     )([1,2,3,4]) == [8,12,16,20]
+
+
+    def getattrs(obj, *attrs):
+        def _getattr(acc, prop):
+            return getattr(acc, prop) if acc is not None else None
+        return reduce(_getattr, attrs, obj)
+
+
+    head = Node(1, 1)
+    head.right = Node(2,2)
+    head.right.right = Node(3,3)
+
+
+    getattr(head, 'right')
+    print(getattrs(head, 'right', 'right', 'val'))
 
 
 def test_cache():
