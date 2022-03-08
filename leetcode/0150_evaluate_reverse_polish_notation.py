e28f29c4af4fed7ede5ddd16b8d39b3faa6bc38a
Justin Hoyt <justinhoyt24@gmail.com>
2022-03-08 15:27:22 -0800

    Add evaluate_reverse_polish_notation solution

--- leetcode/0150_evaluate_reverse_polish_notation.py
+++ leetcode/0150_evaluate_reverse_polish_notation.py
@@ -1,26 +1,45 @@
 from collections import deque
+from operator import mul, add, sub
+from typing import Callable, List
+from math import trunc
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        if len(tokens) < 2:
-            return -1
-
+        token_to_operator = {
+            '*': mul,
+            '+': add,
+            '-': sub,
+            '/': lambda a, b: trunc(a / b)
+        }
+        flip = lambda fn: lambda *args: fn(*reversed(args))
         stack = deque()
 
-        result = int(tokens[0])
-
-        for i range(1, len(tokens)):
-            if token.isnumeric():
-                stack.append(token)
+        for i, token in enumerate(tokens):
+            if token.lstrip('-').isnumeric():
+                stack.append(int(token))
             else:
+                op = token_to_operator[token]
+                stack.append(flip(op)(stack.pop(), stack.pop()))
 
-
+        return stack.pop()
 
 
 def test_happy_path():
     assert Solution().evalRPN(["2","1","+","3","*"]) == 9
 
 
+def test_div():
+    assert Solution().evalRPN(["4","13","5","/","+"]) == 6
+
+
+def test_complex_case():
+    assert Solution().evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]) == 22
+
+
+def test_single_token():
+    assert Solution().evalRPN(["4"]) == 4
+
+
 if __name__ == "__main__":
-    test_happy_path()
+    test_complex_case()
 
