1d291b31a30f0e8adbacdde916710c443f2acba1
Justin Hoyt <justinhoyt24@gmail.com>
2022-03-17 02:31:41 -0700

    Add combinations solution

---
+++ leetcode/0077_combinations.py
@@ -0,0 +1,44 @@
+class Solution:
+    def combine(self, n: int, k: int):
+        combinations = []
+        def generate_combinations(idx: int, sofar: list[int]):
+            if len(sofar) == k:
+                combinations.append(sofar[:])
+                return
+
+            for i in range(idx, n+1):
+                sofar.append(i)
+                generate_combinations(i + 1, sofar)
+                sofar.pop()
+
+        generate_combinations(1, [])
+        return combinations
+
+
+    def combine_built_in(self, n: int, k: int):
+        from itertools import combinations
+        return combinations(list(range(1,n+1)), k)
+
+
+def test_happy_path():
+    assert sorted(Solution().combine(4, 2)) == sorted([
+        [2,4],
+        [3,4],
+        [2,3],
+        [1,2],
+        [1,3],
+        [1,4],
+    ])
+    assert sorted(Solution().combine_built_in(4, 2)) == sorted([
+        (2,4),
+        (3,4),
+        (2,3),
+        (1,2),
+        (1,3),
+        (1,4),
+    ])
+
+
+if __name__ == "__main__":
+    test_happy_path()
+
